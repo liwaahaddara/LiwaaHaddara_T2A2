d07cb8b7312c26bf3c7cf979ef88e097de406b9f
liwaahaddara <12786@coderacademy.edu.au>
2022-10-02 10:03:58 +1100

    added updating&deleting methods to 'Club' model

--- src/controllers/clubs_controllers.py
+++ src/controllers/clubs_controllers.py
@@ -15,14 +15,19 @@ def get_clubs():
     return jsonify(result)
 
 
+# Searching for a single, exisiting club in the database
 @clubs.route("/<int:id>", methods=["GET"])
 def get_club(id):
     # retrieve a specific club from the database
     club = Club.query.get(id)
+    # check if club exist in the database
+    if not club:
+        return {"error": "club id not found in the database"}, 404
     result = club_schema.dump(club)
     return jsonify(result)
 
 
+# Adding a new Club to the Database
 @clubs.route("/", methods=["POST"])
 def new_club():
     club_fields = club_schema.load(request.json)
@@ -36,3 +41,42 @@ def new_club():
     db.session.commit()
 
     return jsonify(club_schema.dump(club))
+
+
+# Updating the attributes of an exisiting Club
+@clubs.route("/<int:id>", methods=["PUT"])
+def update_club(id):
+    # find the club in the database
+    club = Club.query.get(id)
+    # check if club exist in the database
+    if not club:
+        return {"error": "club id not found in the database"}, 404
+    # get the club details from the request
+    club_fields = club_schema.load(request.json)
+    # upodate the values of the club
+    club.club_name = club_fields["club_name"]
+    club.city_located = club_fields["city_located"]
+    club.year_established = club_fields["year_established"]
+
+    # save changes in the database
+    db.session.commit()
+
+    return jsonify(club_schema.dump(club)), 201
+
+
+# Deleting an exisiting Club from the database
+@clubs.route("/<int:id>", methods=["DELETE"])
+def delete_club(id):
+    # search club by id (primary key)
+    club = Club.query.get(id)
+
+    # check if we found a Club
+    if not club:
+        return {"error": "club_id not found"}
+
+    # delete the club from the database
+    db.session.delete(club)
+    # save the changes in the database
+    db.session.commit()
+
+    return {"message": "Club removed successfully"}
