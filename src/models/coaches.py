c02fd86a666ec77d6fef0bf0300b8582e3ac9a18
liwaahaddara <12786@coderacademy.edu.au>
2022-10-01 21:33:18 +1000

    added the 'Coach' model

---
+++ src/models/coaches.py
@@ -0,0 +1,13 @@
+from main import db
+
+
+class Coach(db.Model):
+    # defining the tablename in the database as 'coaches'
+    __tablename__ = "coaches"
+
+    # setting the attributes of the Coach Entity
+    coach_id = db.Column(db.Integer, primary_key=True)
+    c_first_name = db.Column(db.String())
+    c_last_name = db.Column(db.String())
+    coach_type = db.Column(db.String())
+    year_coached = db.Column(db.Integer)
