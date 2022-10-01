b66f6c6badb50e4ce1ca99e4c3c0133077fb09ff
liwaahaddara <12786@coderacademy.edu.au>
2022-10-02 10:16:08 +1100

    added PUT&DELETE methods for 'Coach' model

--- src/controllers/coaches_controller.py
+++ src/controllers/coaches_controller.py
@@ -19,10 +19,14 @@ def get_coaches():
 def get_coach(id):
     # retrieve a specific coach from the database
     coach = Coach.query.get(id)
+    # check if coach exist in the database
+    if not coach:
+        return {"error": "coach id not found in the database"}, 404
     result = coach_schema.dump(coach)
     return jsonify(result)
 
 
+# Adding a new Coach to the Database
 @coaches.route("/", methods=["POST"])
 def new_coach():
     coach_fields = coach_schema.load(request.json)
@@ -38,3 +42,44 @@ def new_coach():
     db.session.commit()
 
     return jsonify(coach_schema.dump(coach))
+
+
+# Updating the attributes of an exisiting Coach
+@coaches.route("/<int:id>", methods=["PUT"])
+def update_coach(id):
+    # find the coach in the database
+    coach = Coach.query.get(id)
+    # check if coach exist in the database
+    if not coach:
+        return {"error": "coach id not found in the database"}, 404
+    # get the coach details from the request
+    coach_fields = coach_schema.load(request.json)
+    # upodate the values of the coach
+    coach.c_first_name = coach_fields["c_first_name"]
+    coach.c_last_name = coach_fields["c_last_name"]
+    coach.coach_type = coach_fields["coach_type"]
+    coach.years_coached = coach_fields["years_coached"]
+    coach.club_id = coach_fields["club_id"]
+
+    # save changes in the database
+    db.session.commit()
+
+    return jsonify(coach_schema.dump(coach)), 201
+
+
+# Deleting an exisiting Coach from the database
+@coaches.route("/<int:id>", methods=["DELETE"])
+def delete_coach(id):
+    # search coach by id (primary key)
+    coach = Coach.query.get(id)
+
+    # check if we found a Coach
+    if not coach:
+        return {"error": "coach_id not found"}
+
+    # delete the coach from the database
+    db.session.delete(coach)
+    # save the changes in the database
+    db.session.commit()
+
+    return {"message": "Coach removed successfully"}
