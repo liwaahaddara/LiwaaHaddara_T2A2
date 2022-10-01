a6dfb41873662447578332b2c1b155176ca65d4b
liwaahaddara <12786@coderacademy.edu.au>
2022-10-01 22:50:49 +1000

    created a 'schemas' sub-folder and added all subsequent schema python files of each Model

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
+ma = Marshmallow()
 
 
 def create_app():
@@ -11,6 +13,7 @@ def create_app():
     app.config.from_object("config.app_config")
 
     db.init_app(app)
+    ma.init_app(app)
 
     from commands import db_commands
     app.register_blueprint(db_commands)
