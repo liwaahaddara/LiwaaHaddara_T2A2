cae140f4a05d5f0a91787358ade40463bc77594c
liwaahaddara <12786@coderacademy.edu.au>
2022-10-01 21:50:00 +1000

    created and instantiated the 'commands.py' file

---
+++ src/commands.py
@@ -0,0 +1,48 @@
+from flask import Blueprint
+from main import db
+
+db_commands = Blueprint("db", __name__)
+
+
+@db.cli.command('create')
+def create_db():
+    # Tell SQLAlchemy to create all tables for all AFL models in the physical DB
+    db.create_all()
+    print('Tables created')
+
+
+@db.cli.command('drop')
+def drop_db():
+    # Tell SQLAlchemy to drop all tables
+    db.drop_all()
+    print('Tables dropped')
+
+
+# @db.cli.command('seed')
+# def seed_db():
+
+#     # Create a new Card (in memory)
+#     card = Card(
+#         title="Start the project",
+#         description="Stage 1 - create db",
+#         status="To Do",
+#         priority="High",
+#         date=date.today()
+#     )
+
+#     # Add the new card to the current transaction (in memory)
+#     db.session.add(card)
+
+#     admin = User(
+#         email="admin@email.com",
+#         # password = "12345678",
+#         password=bcrypt.generate_password_hash("12345678").decode("utf-8"),
+#         admin=True
+#     )
+
+#     db.session.add(admin)
+
+#     # Commit the transaction to the physical DB
+#     db.session.commit()
+
+#     print('Table seeded')
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
 def create_app():
