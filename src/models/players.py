56768ad9c53742135154ee4b67853e99b62f9b68
liwaahaddara <12786@coderacademy.edu.au>
2022-10-01 21:39:32 +1000

    added the 'Player' model

---
+++ src/models/players.py
@@ -0,0 +1,14 @@
+from main import db
+
+
+class Player(db.Model):
+    # defining the tablename in the database as 'players'
+    __tablename__ = "players"
+
+    # setting the attributes of the Player Entity
+    player_id = db.Column(db.Integer, primary_key=True)
+    p_first_name = db.Column(db.String())
+    p_last_name = db.Column(db.String())
+    p_dob = db.Column(db.Date())
+    p_salary = db.Column(db.Integer)
+    years_served = db.Column(db.Integer)
