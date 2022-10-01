091e773fa6d57ef3195e0e6fc97e1c76a313295c
liwaahaddara <12786@coderacademy.edu.au>
2022-10-02 00:30:43 +1000

    added 'get_club()' & 'new_club()' methods

--- src/controllers/clubs_controllers.py
+++ src/controllers/clubs_controllers.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from main import db
 from models.clubs import Club
 from schemas.club_schema import club_schema, clubs_schema
@@ -14,3 +14,26 @@ def get_clubs():
     clubs_list = Club.query.all()
     result = clubs_schema.dump(clubs_list)
     return jsonify(result)
+
+
+@clubs.route("/<int:id>", methods=["GET"])
+def get_club(id):
+    # retrieve a specific club from the database
+    club = Club.query.get(id)
+    result = club_schema.dump(club)
+    return jsonify(result)
+
+
+@clubs.route("/", methods=["POST"])
+def new_club():
+    club_fields = club_schema.load(request.json)
+    club = Club(
+        club_name=club_fields["club_name"],
+        city_located=club_fields["city_located"],
+        year_established=club_fields["year_established"]
+    )
+
+    db.session.add(club)
+    db.session.commit()
+
+    return jsonify(club_schema.dump(club))
