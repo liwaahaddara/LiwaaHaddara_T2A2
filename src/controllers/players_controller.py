526cc46d93454c4f00100d094c8cc60ac96432ab
liwaahaddara <12786@coderacademy.edu.au>
2022-10-02 10:36:08 +1100

    added PUT&DELETE methods for 'Player' model

--- src/controllers/players_controller.py
+++ src/controllers/players_controller.py
@@ -18,11 +18,15 @@ def get_players():
 @players.route("/<int:id>", methods=["GET"])
 def get_player(id):
     # retrieve a specific player from the database
-    player = player.query.get(id)
+    player = Player.query.get(id)
+    # check if player exist in the database
+    if not player:
+        return {"error": "player id not found in the database"}, 404
     result = player_schema.dump(player)
     return jsonify(result)
 
 
+# Adding a new Player to the Database
 @players.route("/", methods=["POST"])
 def new_player():
     player_fields = player_schema.load(request.json)
@@ -39,3 +43,45 @@ def new_player():
     db.session.commit()
 
     return jsonify(player_schema.dump(player))
+
+
+# Updating the attributes of an exisiting Player
+@players.route("/<int:id>", methods=["PUT"])
+def update_player(id):
+    # find the player in the database
+    player = Player.query.get(id)
+    # check if player exist in the database
+    if not player:
+        return {"error": "player id not found in the database"}, 404
+    # get the player details from the request
+    player_fields = player_schema.load(request.json)
+    # update the values of the player
+    player.p_first_name = player_fields["p_first_name"]
+    player.p_last_name = player_fields["p_last_name"]
+    player.p_dob = player_fields["p_dob"]
+    player.p_salary = player_fields["p_salary"]
+    player.club_id = player_fields["club_id"]
+    player.set_of_stats_id = player_fields["set_of_stats_id"]
+
+    # save changes in the database
+    db.session.commit()
+
+    return jsonify(player_schema.dump(player)), 201
+
+
+# Deleting an exisiting Player from the database
+@players.route("/<int:id>", methods=["DELETE"])
+def delete_player(id):
+    # search player by id (primary key)
+    player = Player.query.get(id)
+
+    # check if we found a Player
+    if not player:
+        return {"error": "player_id not found"}
+
+    # delete the player from the database
+    db.session.delete(player)
+    # save the changes in the database
+    db.session.commit()
+
+    return {"message": "Player removed successfully"}
