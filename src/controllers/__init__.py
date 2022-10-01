3cd1b16e80e99395234c3f8c4f75e2824ab58446
liwaahaddara <12786@coderacademy.edu.au>
2022-10-02 05:01:00 +1100

    forgot to include in previous commit

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,5 +1,6 @@
 from controllers.clubs_controllers import clubs
 from controllers.coaches_controller import coaches
 from controllers.doctors_controller import doctors
+from controllers.players_controller import players
 
-registerable_controllers = [clubs, coaches, doctors]
+registerable_controllers = [clubs, coaches, doctors, players]
